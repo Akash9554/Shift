70e606a04b4ec6fd5ec2c6c40d0f208cf88e99b7
Akash <akashgupta95549@gmail.com>
2025-02-05 15:55:43 +0530

    init

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -82,17 +82,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Future<String> getAccessToken() async {
     // Your client ID and client secret obtained from Google Cloud Console
     final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "star-staff-rostering",
-      "private_key_id": "f362291fa6f4cbf883d44f0936b921fc1bb15839",
-      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDZr2+45gXbRzV3\nBHs7mKMI0l+QoVXLvDCo6h7K/PZCSrT6p3yMVzlS/2benGfT5pr5tftJrVe2PC9W\nEPsAmd+W57z4HzzdWhVQOxgkXge3l092NH5VDyGs5UlPKXpfn+EjUIBaTKkJmBOf\no7corXX2RqOArKm5dUmBq55TU2wR76CgGsRj0eL/LT5CnEQugNsWiNMbIPSXwEH2\noUYOS3nFTAnuUGF0oHAwmKd4SbyPB9thmC2DbRlEoI5jitfXCtap5ExwK83rwEkf\nmkkEyE9x5JXlO74A2y1QveXqWypwlSuX607oNGBGEtYJ6j2PQYhJWazwx1UT6OsI\nRbnMMDjzAgMBAAECggEABF6YpAH+ARpLUPHGlXopX551a55Jbhb/dQyKBLISMV91\nhYiCIAEKNgfJZVOHZAH/f16vRhnUJmTL4HLjzu3HxqwtJ1/dJWZPAjbFc8ltZzqC\nzlEcbhGpL+LEV3HyMmQvrTlK8kUSqY4jarNrFEZzFTfyxTzyBTZ8/LzYpzyH8U4w\nFUyNGmAnvVVB/1WJHddsu1/YHdPADU2M5x7N8HhZHTttdcxyYozqxb+xEyDJUXX6\nxh/yLlsnPb5eDfTck+RLZEWs6EQH2p77FuknkTjk6fQmsgG2oy3Bq7DDYhJRFX3U\noj1J4FiEogVKLXTIuq+pXv1tRvKQnIrjlXb6M+3qEQKBgQDvqQBSkEpbuJxkLue7\n7DSJntDVYIWxGCNexsTdlUDa5BamYamPCPHkbPmFZcETNDjAKEFvgyLERKDIb+lr\n8lWU6YtCizhVbjNxiq2IvvL3qIJqyZMgd+ftuRFaxEr+YECJCK3klzV5bToW44jE\nLaZSlArkmf92msrdglYtjQKScQKBgQDohuN9B7rhktnDfDNTWdZG/4lisccrjU/6\ns19EUhCjDE4Q5YGFEQkSGuhvdkt4K12CiarARw+LBbgBDEOdwnuZaC3/GvclEte5\nJcJU+SrYKCRzRmRIIBR1HlodbMMtK+IvJX0HI95PV1qCGBW80SKLf6L+XZ9hUpYA\nOKqgNjQrowKBgQCh1a6ZEkSUIVrCMAZeGQf1MqB0pkRBLTqdQn4n3yS3azR8UBCu\nvOyTQp9QXED/1dDIuaZORfBRNsKX9zJZqX/vGbHnUpnM3qiwcGP0pnTtlgXTVKEc\nWLvuBH3anBjEI30QzCKY7R64C3Ehia6OQwFAXCXq7q7aiXaaE+xbwA/PkQKBgA9S\n7GUOAFvjUKSYZ82DayKsroIpkyWkn3O3JH0dJ2mHYxGr/lglokJmMZE+64RU30m+\nBtm7FSQlUVYIV+Bqs7iNbBKW1VjFICBcg60T6qXqJ1TNe8hhDNgXEvtDcq1kafiP\ncOjqjTkPWbCfI7eY1Lz6wWQgCWPhjG8hTOVYDSIfAoGBAOq4xo2DpNaNJbuHP/jS\n2PXhJj0lcjY/kIJF2Tb7lVycUkq9DZjpaSwFposvXu0sny9X657rIiL5iCskR+Lp\nd2QxOIHE1mkPxUDv66BMQhUb6URPE+looJtZtOoXZ7HAeLkhOXj+ADNj9qbA7re8\nwP8BXMn9iUebuDM2yzeV3pD6\n-----END PRIVATE KEY-----\n",
-      "client_email": "firebase-adminsdk-t7ru9@star-staff-rostering.iam.gserviceaccount.com",
-      "client_id": "105132505600400332663",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t7ru9%40star-staff-rostering.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
+
     };
 
     List<String> scopes = [
